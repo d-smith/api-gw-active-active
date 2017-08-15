673bd08dcd5826ecbea47a90f5d39c475543bc44
d-smith <doug.smith.mail@gmail.com>
2017-08-15 12:46:02 -0700

    Updated build artifacts location, added artifacts generation

--- build/create-build.sh
+++ build/create-build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 aws cloudformation create-stack --stack-name gw-sample-code-build \
 --template-body file://codebuild.yml \
+--parameters ParameterKey=ArtifactBucket,ParameterValue=97068-built-artifacts \
 --capabilities CAPABILITY_IAM
