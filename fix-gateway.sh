3a25bae643037cadd5028eb5ba84b13df646a593
d-smith <doug.smith.mail@gmail.com>
2017-11-06 14:04:38 -0800

    Added patch command to gateway script

--- fix-gateway.sh
+++ fix-gateway.sh
@@ -8,3 +8,4 @@ qs+=$stage
 qs+="\`].{id:id}'"
 id=`eval aws apigateway get-rest-apis --query $qs --output text`
 echo modify $id
+aws apigateway update-rest-api --rest-api-id $id --patch-operations op=replace,path=/endpointConfiguration/types/EDGE,value=REGIONAL
