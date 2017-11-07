fc3ecfe04400fbee40f88265c91e491affa0cca9
d-smith <doug.smith.mail@gmail.com>
2017-11-07 14:07:05 -0800

    Added region to id get as well

--- fix-gateway.sh
+++ fix-gateway.sh
@@ -7,6 +7,6 @@ aws apigateway get-rest-apis
 qs="'items[?name==\`"
 qs+=$stage
 qs+="\`].{id:id}'"
-id=`eval aws apigateway get-rest-apis --query $qs --output text`
+id=`eval aws apigateway get-rest-apis --region $region --query $qs --output text`
 echo modify $id
 aws apigateway update-rest-api --region $region --rest-api-id $id --patch-operations op=replace,path=/endpointConfiguration/types/EDGE,value=REGIONAL
